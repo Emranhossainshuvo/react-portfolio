b7372b02436954a105ea56ee5ef9e217d5c9dc4b
Emranhossain <emran.shuvo.dev@gmail.com>
2024-04-21 20:22:45 +0600

    banner file created and added at the home page

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='bg-[#FEFBE8]'>
     <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             <h3>Hi, this is home</h3>
             <Footer></Footer>
         </div>
