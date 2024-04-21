eeeb4adc0507f425daa5f58a4431a96260e2d025
Emranhossain <emran.shuvo.dev@gmail.com>
2024-04-21 20:35:18 +0600

    created and added the basic banner with one image

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,17 @@
 
 const Banner = () => {
     return (
-        <div>
-                       
-        </div>
+        <>
+            <div className="flex items-center justify-between w-full">
+                <div>
+                    <p>Hi, I'm Md. Emran Hossain</p>
+                    <p>I love creatig functioal web application</p>
+                </div>
+                <div className="w-1/2">
+                    <img src="https://i.ibb.co/9VtwWnR/IMG-20231209-080753-removebg-removebg-preview.png" alt="" />
+                </div>
+            </div>           
+        </>
     );
 };
 
